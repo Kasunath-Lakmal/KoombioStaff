b3fe8888eb01b8b41b1c01277c5fd41cd77c4701
DESKTOP-4V970ER\Koombiyo App <kasunathlakmal.s@gmail.com>
2021-03-24 09:37:35 +0530

    dev_lakmal
    create splash and login screen

---
+++ LoginActivity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace KoombioStaff
+{
+    [Activity(Label = "LoginActivity")]
+    public class LoginActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Loginpage);
+
+           
+
+            // editUsername = FindViewById(R.id.editUsername);
+        }
+
+
+    }
+}
--- MainActivity.cs
+++ MainActivity.cs
@@ -3,6 +3,9 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
+using System.Threading.Tasks;
+using Android.Content;
+using System.Threading;
 
 namespace KoombioStaff
 {
@@ -14,6 +17,37 @@ namespace KoombioStaff
             base.OnCreate(savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+            Task.Factory.StartNew(() => Thread.Sleep(10 * 500))
+                 .ContinueWith((t) =>
+                 {
+                     Intent intent = new Intent(this, typeof(LoginActivity));
+                // intent.PutExtra("IMEI");
+                StartActivity(intent);
+
+                 }, TaskScheduler.FromCurrentSynchronizationContext());
+
         }
+
+
+        //protected override void OnResume()
+        //{
+        //    base.OnResume();
+        //    Task startupWork = new Task(() => { SimulateStartup(); });
+        //    startupWork.Start();
+        //}
+
+
+        //async void SimulateStartup()
+        //{
+        //    await Task.Delay(8000);
+        //    StartActivity(new Intent(Application.Context, typeof(LoginActivity)));
+
+        //}
+
+
+
+
+
+
     }
 }
